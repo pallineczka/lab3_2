8d1f06b25789ec247a955594eabed51ca58a8d19
pallineczka <paulina.stelmasiak97@gmail.com>
2019-06-07 22:19:40 +0200

    add init

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,51 @@
+package edu.iis.mto.staticmock;
+
+import edu.iis.mto.staticmock.reader.NewsReader;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
+
+public class NewsLoaderTest {
+
+    @Mock
+    private ConfigurationLoader configurationLoader;
+    @Mock
+    private NewsReader newsReader;
+    @Mock
+    private NewsReaderFactory newsReaderFactory;
+    @Mock
+    private NewsLoader newsLoader;
+
+    private IncomingNews incomingNews;
+
+    @Before
+    public void init(){
+
+        incomingNews = new IncomingNews();
+        newsLoader = new NewsLoader();
+
+        mockStatic(ConfigurationLoader.class);
+        mockStatic(NewsReaderFactory.class);
+
+        configurationLoader = mock(ConfigurationLoader.class);
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+        when(configurationLoader.loadConfiguration()).thenReturn(new Configuration());
+
+        newsReader = mock(NewsReader.class);
+        when(newsReader.read()).thenReturn(incomingNews);
+
+        newsReaderFactory = mock(NewsReaderFactory.class);
+        when(NewsReaderFactory.getReader(Mockito.any())).thenReturn(newsReader);
+    }
+
+}
